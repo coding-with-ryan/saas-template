3d17e3ada9c6dc7c562a2b6a8ee5b68fffb8c37c
ryan@anvil.works <ryan@anvil.works>
2023-06-19 13:46:42 +0000

    Created HomePage and your-logo-here-low-resolution-logo-color-on-transparent-background.png; Edited settings; Deleted Form1

--- client_code/HomePage/__init__.py
+++ client_code/HomePage/__init__.py
@@ -1,9 +1,14 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import HomePageTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class HomePage(HomePageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
