1dd6ec6d3607ded358df29d702c783e9e61abe71
ryan@anvil.works <ryan@anvil.works>
2023-06-20 15:15:43 +0000

    Created Form1; Edited Main and settings

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -14,3 +14,23 @@ class Main(MainTemplate):
 
     # Any code you write here will run before the form opens.
     self.content_panel.add_component(Home(), full_width_row=True)
+
+    if anvil.users.get_user():
+      self.create_account_button.visible = False
+
+  def create_account_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.login_with_form()
+
+  def login_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.login_with_form()
+
+  def sign_out_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.logout()
+
+
+
+  
+
