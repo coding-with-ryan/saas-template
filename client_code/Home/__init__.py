bb0fa7b6ec50d731bbb7b21ffc55b753cdffa842
ryan@anvil.works <ryan@anvil.works>
2023-06-20 14:03:02 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -10,3 +10,8 @@ class Home(HomeTemplate):
     # Any code you write here will run before the form opens.
     self.features_repeating_panel.items = ({"name": "feature 1"}, {"name": "feature 2"}, {"name": "feature 3"})
 
+  def features_data_grid_show(self, **event_args):
+    """This method is called when the data grid is shown on the screen"""
+    pass
+
+
