ad77223908f7027640f4a0e2493117300d3680f2
ryan@anvil.works <ryan@anvil.works>
2023-06-20 15:18:07 +0000

    Created Pricing; Edited Home; Deleted Form1 and Home.FeatureRow

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -12,7 +12,4 @@ class Home(HomeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.features_repeating_panel.items = ({"name": "feature 1"}, {"name": "feature 2"}, {"name": "feature 3"})
-    self.refresh_data_bindings()
-
 
--- client_code/Pricing/__init__.py
+++ client_code/Pricing/__init__.py
@@ -1,15 +1,14 @@
-from ._anvil_designer import FeatureRowTemplate
+from ._anvil_designer import PricingTemplate
 from anvil import *
+import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
-import anvil.server
 
-class FeatureRow(FeatureRowTemplate):
+class Pricing(PricingTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    
