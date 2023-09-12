98f86847e05c00764c89a230ec87748ee0ffb602
ryan@anvil.works <ryan@anvil.works>
2023-09-12 15:03:25 +0000

    Created SplashPage; Edited settings; Deleted Form1

--- client_code/SplashPage/__init__.py
+++ client_code/SplashPage/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import SplashPageTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,9 +6,18 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class SplashPage(SplashPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def login_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    user = anvil.users.login_with_form()
+    if user:
+      open_form('Main')
+      
+    
+
