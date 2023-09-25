e7d2726e95fe5dc19d4a66cd9b435ea7a453f46a
ryan@anvil.works <ryan@anvil.works>
2023-09-25 11:32:30 +0000

    Edited StripeFunctions

--- server_code/StripeFunctions.py
+++ server_code/StripeFunctions.py
@@ -40,17 +40,29 @@ def stripe_customer_created():
 @anvil.server.http_endpoint('/stripe/stripe_subscription_updated')
 def stripe_subscription_updated():
   # Here we want to look for "customer.subscription.updated" because this event is what shows whether a subscription is valid or not. Events like "customer.subscription.created" are similar but are called before a charge is attempted and is usually followed by "customer.subscription.updated".
+
+  payload_json = json.loads(anvil.server.request.body.get_bytes())
+
+  # Make sure the event is in a format we expect
+  try:
+    event = stripe.Event.construct_from(
+      payload_json, stripe.api_key
+    )
+  except ValueError as e:
+    # Invalid payload
+    return anvil.server.HttpResponse(400)
   
   # Need to get the users record from the DB based on the subscription objects "customer" field
-  payload_json = json.loads(anvil.server.request.body.get_bytes())
-  stripe_customer_id = payload_json.get("object").get("customer")
-  user = app_tables.users.get(stripe_customer_id=stripe_customer_id)
+  print(payload_json)
+  stripe_customer_id = payload_json.get("data").get("object").get("customer")
+  user = app_tables.users.get(stripe_id=stripe_customer_id)
 
   # Check the subscription objects status: https://stripe.com/docs/api/subscriptions/object#subscription_object-status
-  subscription_status = payload_json.get("object").get("status")
+  subscription_status = payload_json.get("data").get("object").get("status")
   # If the subscription status is "Active"
+  print(subscription_status)
   if subscription_status == "Active":
-    price_id_of_plan = payload_json.get("object").get("items").get("plan").get("id")
+    price_id_of_plan = payload_json.get("data").get("object").get("items").get("plan").get("id")
     # Check the price/plan and update the user record in the DB accordingly
     if price_id_of_plan == PRICES["Personal"]:
       user["subscription"] = "Personal"
@@ -58,58 +70,8 @@ def stripe_subscription_updated():
       user["subscription"] = "Pro"
   else:
     user["subscription"] = "Expired"
-  
-  
-  event = None
-  payload = anvil.server.request.body
-  payload_json = anvil.server.request.body_json
-  sig_header = request.headers["STRIPE_SIGNATURE"]
-  endpoint_secret = "we_1NsnudAp4vQdl4epdWPQpKUL"
 
-  price_id = payload_json.get("object").get("items").get("data")[0].get("price").get("id")
-  
-  
-  try:
-    event = json.loads(anvil.server.request.body.get_bytes())
-  except:
-      print("⚠️  Webhook error while parsing basic request." + str(e))
-      return jsonify(success=False)
-  if endpoint_secret:
-    # Only verify the event if there is an endpoint secret defined
-    # Otherwise use the basic event deserialized with json
-    sig_header = request.headers.get("stripe-signature")
-    try:
-      event = stripe.Webhook.construct_event(
-          payload, sig_header, endpoint_secret
-      )
-    except stripe.error.SignatureVerificationError as e:
-      print("⚠️  Webhook signature verification failed" + str(e))
-      return jsonify(success=False)
-
-  # Handle the event
-  if event and (event.get("type") == "customer.subscription.created" or event.get("type") == "customer.subscription.updated"):
-    # GOT TO FIND OUT WHICH USER TO UPDATE FROM THE EVENT DATA subscription.created or . updated -> customer -> customer.metadata.anvil_user_row_id -> (anvil) get_user -> updated subscription column
-    event.get("type")
-    user_row = app_tables.users.get_by_id(user_row_id)
-    stripe_customer_id = payload_json.get("object").get("id") or payload_json.get("object").get("customer")
-    if stripe_customer_id:
-      app_tables.users.get_by_id()
-    else:
-      print("⚠️  No customer ID")
-  else:
-    # Unexpected event type
-    print('Unhandled event type {}'.format(event['type']))
-
-  return jsonify(success=True)
-
-
-# # If Personal plan is selected Personal plan ID: price_1Ns3AAAp4vQdl4epHiqlYaIc
-# if price_id == "price_1Ns3AAAp4vQdl4epHiqlYaIc":
-#   user_row["subscription"] = "Personal"
-#   print("User row subscription: ", user_row['subscription'])
-# elif price_id == "price_1Ns3AbAp4vQdl4epxcEN5RUz":
-#   user_row["subscription"] = "Pro"
-#   print("User row subscription: ", user_row['subscription'])
+  anvil.server.HttpResponse(200)
 
 
 
