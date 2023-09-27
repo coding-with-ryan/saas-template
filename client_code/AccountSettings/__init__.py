db8e7e2eae70dff2a693da1031f0d7c109ea861a
ryan@anvil.works <ryan@anvil.works>
2023-09-27 09:11:28 +0000

    Created AccountSettings; Edited AccountPage

--- client_code/AccountSettings/__init__.py
+++ client_code/AccountSettings/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import AccountPageTemplate
+from ._anvil_designer import AccountSettingsTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,14 +6,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class AccountPage(AccountPageTemplate):
+class AccountSettings(AccountSettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def settings_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
-
