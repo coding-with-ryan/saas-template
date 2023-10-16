2de7da90d0b5687a61e0af9f31362893657321f6
ryan@anvil.works <ryan@anvil.works>
2023-10-16 21:44:31 +0000

    Created APP_README; Edited Main

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -48,6 +48,8 @@ class Main(MainTemplate):
   def TEMPLATE_EXPLANATION(self):
     if anvil.users.get_user()["subscription"] in ["personal", "pro"]:
       Notification("With your subscription set up, you can now use the calculator. Check the Users module in the template's server modules and the client code user_permissions module to see how the user permissions work.", title="Template Explanation", timeout=None, style="warning").show()
-    elif anvil.users.get_user()[""]
+    elif anvil.users.get_user()["cancel_at_period_end"]:
+      Notification("You've cancelled your subscription and, once it expires, your user records subscription status will be updated.", title="Template Explanation", timeout=None, style="warning").show()
+      Notification("That's the tour of the app template complete. Now it's time for you to begin adding your own Stripe account details and finding out how to make the app your own. .", title="Template Explanation", timeout=None, style="warning").show()
     else:
       Notification("This is your SaaS product's main page. For this template, we've created a very simple calculator that requires a subscription to use. Try using the calculator.", title="Template Explanation", timeout=None, style="warning").show()
